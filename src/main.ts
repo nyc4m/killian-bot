63d2b4c0d3d37b86dd785d37243d754004724e93
nyc4m <baptiste.prunot@utt.fr>
2020-08-06 22:19:45 +0200

    solve #3

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,59 @@
 import dotenv from 'dotenv'
-import Telegraf  from 'telegraf'
-import  {MenuTemplate, MenuMiddleware}from 'telegraf-inline-menu'
+import Telegraf from 'telegraf'
+import { MenuTemplate, MenuMiddleware } from 'telegraf-inline-menu'
 import fs from 'fs'
-import {exec} from 'child_process'
+import { exec } from 'child_process'
+import util, { log } from 'util'
+
+const promisifiedExec = util.promisify(exec)
 
 dotenv.config()
 
 const FILTERS = /\.(mp4|mkv)$/
-const ROOT_DIR = process.env.FILES
+const ROOT_DIR = process.env.FILES || ''
 
+const playFile = (rootDir: string, videoName: string) => {
+  return promisifiedExec(
+    `vlc "${rootDir}/${videoName}" --fullscreen --sub-track English --audio-track 1 --aspect-ratio 16x9`
+  )
+}
 
 async function main() {
-    const bot = new Telegraf(process.env.TOKEN || "");
-    const menu = new MenuTemplate(ctx => "Yo, c'est le template")
-    const files = await fs.promises.readdir(ROOT_DIR)
-    const videos = files.filter(file => file.match(FILTERS))
+  const bot = new Telegraf(process.env.TOKEN || '')
+  const menu = new MenuTemplate((ctx) => "Hi, here's your files")
+  const files = await fs.promises.readdir(ROOT_DIR)
+  const videos = files.filter((file) => file.match(FILTERS))
 
-    videos.forEach((video, index) => {
-        menu.interact(video, `file-${index}`,{
-            do: async (ctx: any) => {
-                exec(`vlc "${ROOT_DIR}/${video}" --fullscreen --sub-track English --audio-track 1 --aspect-ratio 16x9`,(err, stdout, stderr) => {
-                    if(err) {
-                        console.error(err)
-                        console.error(`stderr: ${stderr}`)
-                        return
-                    }
-                    console.log(stdout)
-                })
-                return false
-            }
-        })
+  videos.forEach((video, index) => {
+    menu.interact(video, `file-${index}`, {
+      do: async (ctx: any) => {
+        playFile(ROOT_DIR, video).then(() =>
+          fs.writeFile('./last.killian', video, (err) =>
+            log(err?.message || '')
+          )
+        )
+        return true
+      },
     })
+  })
 
-    const menuMiddleware = new MenuMiddleware('/', menu)
-    bot.on('message', ctx => menuMiddleware.replyToContext(ctx))
-    bot.use(menuMiddleware)
-    bot.use()
+  const menuMiddleware = new MenuMiddleware('/', menu)
+  bot.on('message', async (ctx) => {
+    const pendingProcess = menuMiddleware.replyToContext(ctx)
+    const pendingFile = fs.promises.readFile('last.killian')
+    await pendingProcess
+    try {
+      const lastVideo = (await pendingFile).toString('utf8')
+      ctx.reply(`last video was : ${lastVideo}`)
+    } catch (err) {
+      console.error(err.message)
+    }
+  })
+  bot.use(menuMiddleware)
+  bot.use()
 
-    console.log("ça part")
-    bot.launch()
+  console.log('ça part')
+  bot.launch()
 }
 
 main()
